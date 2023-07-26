a35084372a9cdc71ad3aceae92e86a11d7e2cfbb
Aimable Mardel <73969781+aimardel@users.noreply.github.com>
2023-07-26 17:04:06 +0100

    First test written and passed

--- src/utils/dataService.js
+++ src/utils/dataService.js
@@ -2,10 +2,10 @@ import axios from 'axios';
 
 export const getNewsArticle = async () => {
     try {
-        const responseData = await axios.get("https://content.guardianapis.com/search?order-by=newest&show-fields=byline%2Cthumbnail%2Cheadline%2CbodyText&api-key=121530fe-30bb-4e77-af58-f964b4fb4640");
+        const responseData = await axios.get("http://localhost:3000/response");
         return responseData.data;
     } catch (error) {
-        return { error };
+        return error;
     }
 }
 
--- tests/newsDataService.test.js
+++ tests/newsDataService.test.js
@@ -1,2 +1,20 @@
-import axiosMock from 'axios';
+import axios from 'axios';
 import { getNewsArticle } from '../src/utils/dataService';
+import { describe } from 'vitest';
+import testArticles from '../src/constants/mockNewsData.json';
+
+
+vi.mock(`axios`);
+
+describe('getNewsArticle tests', () => {
+    describe('GET request tests to /response', () => {
+        it('1. should actually make the external data call', async () => {
+            // Arrange
+            // Nothing to arrange here. For this test, the data doesn't need to be returned.
+            // Act
+            await getNewsArticle();
+            // Assert
+            expect(axios.get).toHaveBeenCalledWith('http://localhost:3000/response');
+        })
+    })
+})
